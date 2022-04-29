d4c8c08cac86c27d0242da1dfcdf3bd49b0ca100
xtarzyk <xtarzyk@tutanota.com>
2022-04-29 15:25:53 +0200

    added option to change line width and color

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const pencil = document.querySelector('.toolbar__pencil')
+const widthRange = document.querySelector('.width-range')
+const colorPicker = document.querySelector('.color-picker')
 const undo = document.querySelector('.toolbar__undo')
 const canvas = document.getElementById('canvas')
 console.log('canvas', canvas)
@@ -13,6 +15,8 @@ let newShape = []
 
 canvas.setAttribute('width', `${canvas.scrollWidth}`)
 canvas.setAttribute('height', `${canvas.scrollHeight}`)
+// widthRange.value = 1
+// colorPicker.value = 'black'
 
 const getPosition = event => {
   coord.x = event.clientX - canvas.offsetLeft
@@ -36,27 +40,31 @@ const sketch = event => {
   if (!draw) return
   
   ctx.beginPath()
-  // ctx.lineWidth = 2
+  ctx.lineWidth = widthRange.value
+  ctx.strokeStyle = colorPicker.value
   ctx.moveTo(coord.x, coord.y)
   getPosition(event)
   ctx.lineTo(coord.x, coord.y)
   ctx.stroke()
-  newShape = newShape.concat({x: coord.x, y: coord.y})
+  newShape = newShape.concat({x: coord.x, y: coord.y, lineWidth: widthRange.value, color: colorPicker.value})
   
   console.log('newShape', newShape)
 
 }
 
 const drawLines = () => {
-  shapes.forEach(arr => {
+  shapes.forEach((arr, index) => {
     ctx.beginPath()
+    ctx.moveTo(arr[index].x, arr[index].y)
     arr.forEach((coord, index) => {
-      ctx.moveTo(coord.x, coord.y)
-      console.log(index)
+      ctx.lineWidth = coord.lineWidth
+      ctx.strokeStyle = coord.color
+      // ctx.moveTo(coord.x, coord.y)
+      // console.log(index)
       if (index < arr.length - 1) {
         ctx.lineTo(arr[index + 1].x, arr[index + 1].y)
-        ctx.stroke()
-        ctx.closePath()
+        // ctx.stroke()
+        // ctx.closePath()
         return
       }
       ctx.lineTo(arr[index].x, arr[index].y)
